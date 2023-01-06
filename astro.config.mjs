2286b60383779e7417ceb2982e19dd79832a37ae
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-01-06 14:04:50 +0100

    add syntax highlighting to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'nord',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: ['c', 'js', 'ts'],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: false,
+    },
+  },
+}
+);

32278dac3c7028ad8ca620c4aa4bad6c6ecf77b7
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-02-28 17:30:04 +0100

    better color highlighting for code

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,11 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'nord',
+      theme: 'poimandres',
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: ['c', 'js', 'ts'],
+      langs: ['c', 'js', 'ts', 'rust', 'cpp'],
       // Enable word wrap to prevent horizontal scrolling
       wrap: false,
     },
